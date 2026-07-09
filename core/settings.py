00567ff195c9d24dd25ef0e5710d2af9738a720d
khakimovallamurod <allamurod0206@gmail.com>
2026-07-09 12:28:22 +0500

    add fixed kasb yoriqnoma

--- core/settings.py
+++ core/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
