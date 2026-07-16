cc80bc40ac7e2b3050560c693ba8d1caa9ac898f
khakimovallamurod <allamurod0206@gmail.com>
2026-07-16 17:51:08 +0500

    fixed ip

--- core/settings.py
+++ core/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'safeworkdb',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': '127.0.1.17',
+        'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
